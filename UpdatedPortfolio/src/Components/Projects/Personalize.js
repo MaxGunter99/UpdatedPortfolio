3a77831bfe135690140f51912eea79fd8b0b88ac
maxgunter99 <maxgunter99@gmail.com>
2020-05-14 21:14:26 -0500

    updated personalize

--- UpdatedPortfolio/src/Components/Projects/Personalize.js
+++ UpdatedPortfolio/src/Components/Projects/Personalize.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
 // IMAGES
-import PersonalizeImg from '../../Images/personalize.jpeg';
+import PersonalizeImg from '../../Images/Personalize.jpg';
 import '../../css/Personalize.css'
 
 export default class Personalize extends React.Component {
 
-    toGitHub = () => {
+    toGitHubFrontEnd = () => {
 
-        window.location = 'https://github.com/MaxGunter99/Personalize'
+        window.location = 'https://github.com/MaxGunter99/Personalize-FrontEnd'
+
+    }
+
+    toGitHubBackEnd = () => {
+
+        window.location = 'https://github.com/MaxGunter99/Personalize-FrontEnd'
 
     }
 
@@ -30,11 +36,15 @@ export default class Personalize extends React.Component {
                         
                         <ul>
                             <li>Keeps track of all the jobs a user applies to.</li>
+                            <li>Implemented a web scraper to gather the best recommendations for the user to apply to from their favorite job boards. If the user wants to apply to a listed job, all of the necessary information is already provided for easy addition to their database.</li>
                             <li>Visual activity graph based on how many jobs the a user applies to per week.</li>
                             <li>Node backend used to store all the users data.</li>
                         </ul>
 
-                        <button className = 'GitHub' onClick = { () => this.toGitHub() }>View on GitHub</button>
+                        <div className = 'gitHubSection'>
+                            <button className = 'GitHub' onClick = { () => this.toGitHubFrontEnd() }>GitHub Front-End</button>
+                            <button className = 'GitHub' onClick = { () => this.toGitHubBackEnd() }>GitHub Back-End</button>
+                        </div>
 
                     </div>
 
